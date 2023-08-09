e373be88f68b1cb76a9b1756d76f4911d8d6fe90
Bernardo da Silva Pathecki <bernardopathecki.pessoal@gmail.com>
2023-08-08 22:37:12 -0300

    up pro Git

--- JovemProgramadorWeb/Controllers/AlunoController.cs
+++ JovemProgramadorWeb/Controllers/AlunoController.cs
@@ -1,11 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JovemProgramadorWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
 namespace JovemProgramadorWeb.Controllers
 {
     public class AlunoController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public AlunoController(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Aluno() 
         { 
             return View();
+        }
+        public IActionResult AdicionarAluno()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> BuscarEndereco(string cep)
+        {
+            Endereco endereco = new Endereco();
+
+            try 
+            {
+                cep = cep.Replace("-", "");
+
+                using var client = new HttpClient();
+                var result = await client.GetAsync(_configuration.GetSection("ApiCep")["BaseUrl"]+ cep + "/json");
+                if (result.IsSuccessStatusCode)
+                {
+                    endereco = JsonSerializer.Deserialize<Endereco>(await result.Content.ReadAsStringAsync(), new JsonSerializerOptions());
+                }
+                else
+                { 
+                    
+                }
+            } 
+            catch (Exception ex) 
+            { 
+                throw; 
+            }
+
+            return View("Index");
         }
     }
 }
